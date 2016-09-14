4b255e79eeeb9d3be3fd207b3a255d01fa906c27
Daniel de Andrade Varela <professor.dav@gmail.com>
2016-09-13 22:32:41 -0300

    iniciada a criacao a interface grafica do chat

--- back/source/main.ts
+++ back/source/main.ts
@@ -1,48 +1,48 @@
-import Pessoa from './classes/pessoa.class';
+// import Pessoa from './classes/pessoa.class';
 
-var x = "Assassinhora!";
+// var x = "Assassinhora!";
 
-Pessoa.testeMySQL().then(
-	function(rows){
-		x = rows;
-	}, 
+// Pessoa.testeMySQL().then(
+// 	function(rows){
+// 		x = rows;
+// 	}, 
 
-	function(err){
-		throw err;
-	}
-);
+// 	function(err){
+// 		throw err;
+// 	}
+// );
 
 
-setTimeout(function() {
-	console.log(x);
-}, 10 * 1000);
+// setTimeout(function() {
+// 	console.log(x);
+// }, 10 * 1000);
 
-console.log(x);
+// console.log(x);
 
-// import * as http from 'http';
-// import * as WebSocket from 'ws';
-// import Rest from './classes/rest.class';
+import * as http from 'http';
+import * as WebSocket from 'ws';
+import Rest from './classes/rest.class';
 
-// const WebSocketServer = WebSocket.Server;
-// const httpServer = http.createServer();
-// const wss = new WebSocketServer({server: httpServer});
-// const rest = new Rest();
+const WebSocketServer = WebSocket.Server;
+const httpServer = http.createServer();
+const wss = new WebSocketServer({server: httpServer});
+const rest = new Rest();
 
-// (<any>wss).broadcast = function(data) {
-//   wss.clients.forEach( (client) => {
-//     client.send(data);
-//   });
-// };
+(<any>wss).broadcast = function(data) {
+  wss.clients.forEach( (client) => {
+    client.send(data);
+  });
+};
 
-// wss.on('connection', (conexaoAtual) => {
-// 	conexaoAtual.send('Conexão estabelecida!');
-// 	conexaoAtual.on('message', (message) => {
-// 		wss.broadcast(message);
-// 	});
-// });
+wss.on('connection', (conexaoAtual) => {
+	conexaoAtual.send('Conexão estabelecida!');
+	conexaoAtual.on('message', (message) => {
+		wss.broadcast(message);
+	});
+});
 
-// httpServer.on("request", rest.expressInstance);
+httpServer.on("request", rest.expressInstance);
 
-// httpServer.listen("8080", () => {
-// 	console.log('Ouvindio em ' + httpServer.address().port)
-// });
+httpServer.listen("8080", () => {
+	console.log('Ouvindio em ' + httpServer.address().port)
+});
